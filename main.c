dd83c44514f0c1e8835a05f6188e809669a06d5f
heewonkim19 <55428337+heewonkim19@users.noreply.github.com>
2019-11-20 01:05:23 +0900

    2019. 11. 19. TUE (10)

--- main.c
+++ main.c
@@ -30,12 +30,13 @@ int cardSum[N_MAX_USER];					//sum of the cards
 int bet[N_MAX_USER];						//current betting 
 int gameEnd = 0; 							//game end flag
 
-//card processing functions -------------------------------------------------------------------------------
+//Card processing functions -------------------------------------------------------------------------------
 
-//calculate the actual card number in the blackjack game
-int getCardNum(int cardnum) {
-
-	int input;  
+//Calculate the actual card number in the blackjack game
+int getCardNum(int cardnum, int user) {
+	
+	int input;
+	
 	if (cardnum <= 9)
 		input = cardnum + 1;
 	else if (cardnum >= 13 && cardnum <= 22)
@@ -46,12 +47,20 @@ int getCardNum(int cardnum) {
 		input = cardnum %38;
 	else 
 		input = 10;
+		
+	if (input == 1)
+	{
+		if(cardSum[user] <= 10)
+			input = 11;
+		else 
+			input = 1;
+	}
 	
 	return input;
 
 }
 
-//print the card information (e.g. DiaA)
+//Print the card information (e.g. DiaA)
 void printCard(int cardnum) {
 	switch (cardnum)
 	{
@@ -69,15 +78,15 @@ void printCard(int cardnum) {
 		case 11:
 		case 12:
 		if (cardnum==0)
-			printf("Hart A");
+			printf(" Hart A ");
 		else if (cardnum==10)
-			printf ("Hart J");
+			printf (" Hart J ");
 		else if (cardnum==11)
-			printf("Hart Q");
+			printf(" Hart Q ");
 		else if (cardnum==12)
-			printf("Hart K");	
+			printf(" Hart K ");	
 		else	
-			printf("Hart %d",cardnum+1);
+			printf(" Hart %d ",cardnum+1);
 		break;
 		
 		case 13:
@@ -94,15 +103,15 @@ void printCard(int cardnum) {
 		case 24:
 		case 25:
 		if (cardnum==13)
-			printf("Dia A");
+			printf(" Dia A ");
 		else if (cardnum==23)
-			printf("Dia J");
+			printf(" Dia J ");
 		else if (cardnum==24)
-			printf("Dia Q");
+			printf(" Dia Q ");
 		else if (cardnum==25)
-			printf("Dia K");
+			printf(" Dia K ");
 		else
-			printf("Dia %d",cardnum%12);
+			printf(" Dia %d ",cardnum%12);
 		break;	
 		
 		case 26:
@@ -119,15 +128,15 @@ void printCard(int cardnum) {
 		case 37:
 		case 38:
 		if (cardnum==26)
-			printf("Spade A");
+			printf(" Spade A ");
 		else if (cardnum==36)
-			printf("Spade J");
+			printf(" Spade J ");
 		else if (cardnum==37)
-			printf("Spade Q");
+			printf(" Spade Q ");
 		else if (cardnum==38)
-			printf("Spade K");
+			printf(" Spade K ");
 		else
-			printf("Spade %d", cardnum%25);
+			printf(" Spade %d ", cardnum%25);
 		break;
 		
 		case 39:
@@ -144,21 +153,23 @@ void printCard(int cardnum) {
 		case 50:
 		case 51:
 		if (cardnum==39)
-			printf("Club A");
+			printf(" Club A ");
 		else if (cardnum==49)
-			printf("Club J");
+			printf(" Club J ");
 		else if (cardnum==50)
-			printf("Club Q");
+			printf(" Club Q ");
 		else if (cardnum==51)
-			printf("Club K");
+			printf(" Club K ");
 		else
-			printf("Club %d", cardnum%38);
+			printf(" Club %d ", cardnum%38);
 		break;	
 		}	
+		
+	return;
 }
 
-//card array controllers -------------------------------------------------------------------------------------------------------------------------------------- 
-//mix the card sets and put in the array
+//Card array controllers -------------------------------------------------------------------------------------------------------------------------------------- 
+//Mix the card sets and put in the array
 void mixCardTray(void) {
 
 	int i, j;
@@ -176,9 +187,142 @@ void mixCardTray(void) {
 		CardTray[j] = CardTray[k];
 		CardTray[k] = temp;
 	}
+	
+	return;
 }
 
+//Get one card from the tray
+int pullCard(void) {
+ 
+	int pull_card;  
+	
+	pull_card = CardTray[cardIndex];
+	cardIndex++;
+	
+	if (cardIndex >= 48)
+		gameEnd = 1;
+	
+	return pull_card;	
+}
 
+//Playing game functions -------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//Player settiing
+int configUser(void) {
+
+	do {
+	printf("input the number of player(Max:5): ");
+	scanf("%d", &n_user);
+	
+	if (N_MAX_USER < n_user)
+		printf("\nToo Many Players !!\n");
+		
+	}while(N_MAX_USER < n_user);
+	
+	return n_user;
+}
+
+//Betting
+void betDollar(void) {
+
+	int i;
+	
+	printf("\n\n-----------------BETTING STEP---------------------\n");
+	
+	printf(" -> Your betting(total: %d): ", dollar[0]); //This is user's betting (Player 0) 
+	scanf("%d", &bet[0]);
+
+	for (i=1; i<n_user; i++) // This is computer players 
+	{
+		bet[i] = 1+(rand()%N_MAX_BET);
+		printf("\n -> player%d's betting: %d (out of %d)\n", i, bet[i], dollar[i]);
+	}
+	
+	return; 
+}
+
+//Offering initial 2 cards
+void offerCards(void) {
+
+	int i;
+	
+	//1. Give two card for each players
+	for (i=0;i<n_user;i++)
+	{
+		cardhold[i][0] = pullCard();
+		cardhold[i][1] = pullCard();
+	}
+
+	//2. Give two card for the operator
+	cardhold[n_user][0] = pullCard();
+	cardhold[n_user][1] = pullCard();
+	
+	return;
+}
+
+//Print initial card status
+void printCardInitialStatus(void) {
+   
+   int i;
+
+    printf("-> dealer: X ");
+	printCard(cardhold[n_user][1]);
+
+	printf("\n-> you: ");
+	printCard(cardhold[0][0]);
+	printCard(cardhold[0][1]);
+   
+	for(i=1; i<n_user-1; i++)
+	{
+	printf("\n-> player%d: ",i);
+    printCard(cardhold[i][0]);
+    printCard(cardhold[i][1]);
+	}
+	
+   return;
+}
+
+//Ask "Go" or "Stop" to player
+int getAction(int user){
+	
+	int action;
+	
+	if (user == 0)
+	{
+		printf("\t ::: Action?(0-Go, other integer-Stop)");
+		scanf("%d", &action);
+	}
+	else 
+	{
+		if (cardSum[user] < 17)
+		{
+			printf("\t ::: GO!");
+			printf("\n");			
+			action = 0;
+		}
+		else
+		{
+			printf("\t ::: STAY!");
+			printf("\n");
+			action = 1;
+		}
+	}
+
+	return action;  
+}
+
+//Print user's card status
+void printUserCardStatus(int user, int cardcnt) {
+	
+	int i;
+	
+	printf("   -> card : ");
+	for (i=0; i<cardcnt; i++)
+		printCard(cardhold[user][i]);
+	printf("\t ::: ");
+	
+	return;
+}
 
 
 int main(int argc, char *argv[]) {
